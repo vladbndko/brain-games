e785421cfab917e06bae5a1fc4c6d178a074acac
Vladyslav Bondarenko <vlad.bond05@gmail.com>
2019-12-18 17:07:02 +0200

    prime.js#L7

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,13 +3,16 @@ import getRandomNum from '../utilities';
 
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-export const isPrime = (num) => {
-  for (let i = 2; i < num; i + 1) {
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
-  return num > 1;
+  return true;
 };
 
 const generatePrimeGame = () => {
