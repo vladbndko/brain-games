e5b6d1537e05ffe4df0491c8d72473534e87d007
Vladyslav Bondarenko <vlad.bond05@gmail.com>
2019-12-18 16:38:38 +0200

    progression.js#L5

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,7 +2,7 @@ import engine from '..';
 
 const gameRule = 'What number is missing in the progression?';
 
-const generateNumber = (start, step, length) => {
+const generateProgressionNumbers = (start, step, length) => {
   const iter = (num, len, acc) => {
     if (len === 0) {
       return acc;
@@ -14,7 +14,7 @@ const generateNumber = (start, step, length) => {
 };
 
 const generateProgressionGame = () => {
-  const numbers = generateNumber(5, 2, 10);
+  const numbers = generateProgressionNumbers(5, 2, 10);
   const target = numbers[Math.floor(Math.random() * 10)];
   const question = numbers.slice().join(' ').replace(target, '..');
   const correctAnswer = target.toString();
