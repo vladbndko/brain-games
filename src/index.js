8d6735e5644e4933fc9be5e634f96415fa43778b
Vladyslav Bondarenko <vlad.bond05@gmail.com>
2019-12-24 21:00:49 +0200

    /src/index.js#L3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import readlineSync from 'readline-sync';
 
-const maxRounds = 3;
+const maxCounter = 3;
 
 export default (rule, getData) => {
   console.log('Welcome to the Brain Games!\n');
   console.log(`${rule}\n`);
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!\n`);
-  const game = (currentRounds) => {
-    if (currentRounds === maxRounds) {
+  const startGame = (counter) => {
+    if (counter === maxCounter) {
       console.log(`Congratulations, ${name}!`);
       return;
     }
@@ -17,11 +17,11 @@ export default (rule, getData) => {
     const answer = readlineSync.question('Your answer: ');
     if (answer === correct) {
       console.log('Correct!\n');
-      game(currentRounds + 1);
+      startGame(counter + 1);
     } else {
       console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correct}".\n`);
       console.log(`Let's try again, ${name}!`);
     }
   };
-  game(0);
+  startGame(0);
 };
